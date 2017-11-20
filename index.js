32c9b536c0bc15dc197895e481f11266d555f65e
Shea Lignitz <rlignitz@gmail.com>
2017-11-20 21:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
+
+function appendDriver(name) {
+  return let newDrivers = [name, ...drivers];
+}
